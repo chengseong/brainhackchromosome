21e794687c5d14f72c6c42141f9d3be95851f7d1
chengseong <chengseong02@hotmail.com>
2021-06-15 21:43:58 +0800

    Added Notification Modal

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { NavigationContainer  } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 
 import HomeStack from './routes/home.js';
@@ -24,7 +25,29 @@ export default function App() {
   if (fontsLoaded) {
     return (
       <NavigationContainer> 
-        <Tab.Navigator initialRouteName = "Home">
+        <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'HomeStack') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'ComponentStack') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'gray',
+          inactiveTintColor: 'gray',
+          activeBackgroundColor: '#0009ff',
+        }}
+      >
           <Tab.Screen name="HomeStack" component={HomeStack} />
           <Tab.Screen name="ComponentStack" component={ComponentStack} />
         </Tab.Navigator>
