9212f4514f81065b9b198116680ef600f71f119f
fsayhan123 <fsayhan123@gmail.com>
2021-06-15 20:26:03 +0800

    Complete Booking Frontend

--- App.js
+++ App.js
@@ -11,8 +11,8 @@ import HomeStack from './routes/home.js';
 import ComponentStack from './routes/component.js'
 import loginStack from './routes/login.js';
 
-import Consult from './components/bookConsult3.js'
-import Login from './components/loginPage.js'
+import Consult from './components/bookConsult2'
+//import Login from './components/loginPage.js'
 
 const Drawer = createDrawerNavigator();
 
--- components/bookConsult2.js
+++ components/bookConsult2.js
@@ -1,14 +1,22 @@
 import * as React from 'react';
-import {View, Text, StyleSheet, Dimensions, ScrollView} from 'react-native';
+import {View, Text, StyleSheet, TextInput, ScrollView} from 'react-native';
 import DatePicker from 'react-native-modern-datepicker';
 import { getToday } from 'react-native-modern-datepicker';
+import {Picker} from '@react-native-picker/picker';
 import {Button} from 'react-native-paper';
 
 
 function bookConsult2() {
-    const [date, setDate] = React.useState(new Date())
-    const [displayTime, setDisplayTime] = React.useState(false)
+    const timeSlotArr = ["2.30pm - 3.00pm", "3.00pm - 3.30pm"] //Need to pull api from clinic and date after date selected 
     const currDate = getToday().replace("/", "-").replace("/", "-")
+
+    const [date, setDate] = React.useState('')
+    const [dateSelected, setDateSelected] = React.useState(false)
+    const [selectedTime, setSelectedTime] = React.useState(timeSlotArr[0])
+    const [description, setDescription] = React.useState("")
+    
+    
+    
 
     return(
         <ScrollView style = {{flexGrow:1}}>
@@ -23,16 +31,29 @@ function bookConsult2() {
                     Select a Date
                 </Text>
                 <DatePicker 
-                    value = {new Date()}
                     mode = "calendar"
                     minimumDate = {currDate}
-                    onDateChange = {(newDate) => {setDate(newDate);}}
+                    onDateChange = {(newDate) => {setDate(newDate);setDateSelected(true)}}
                 />
             </View>
+            {dateSelected &&
             <View style = {{flex: 0.4, marginLeft:30, width:"80%"}}>
                 <Text style = {styles.subHeaderText}>Select a Time</Text>
+                <Picker 
+                    mode = "dropdown"
+                    selectedValue = {selectedTime}
+                    onValueChange = {(selectedTime) => {setSelectedTime(selectedTime)}}
+                    >
+                    {timeSlotArr.map(item => (
+                      <Picker.Item label = {item} value = {item}/>  
+                    ))}
+                    </Picker>
+                <Text style = {{fontSize:16}}>Give a brief description of your illness:</Text>
+                <TextInput value = {description} onChangeText = {(text) => setDescription(text)}/>
+            </View>}
+            <View style = {{flex:0.1, alignItems:'center', justifyContent:'center', marginTop:30}}>
+                <Button mode = "contained" onPress = {() => {console.log(date); console.log(selectedTime); console.log(description)}}>Next</Button>
             </View>
-            <View style = {{flex:0.1, alignItems:'center', justifyContent:'center'}}><Button mode = "contained">Next</Button></View>
         </View>
         </ScrollView>
     );
